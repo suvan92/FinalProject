433ee938b4e81706361f2d269369b11a7450498e
Suvan <suvanramani@gmail.com>
2016-12-15 17:27:41 -0800

    Create user model

--- FinalProject/User.swift
+++ FinalProject/User.swift
@@ -10,7 +10,17 @@ import UIKit
 
 class User: NSObject {
     
+    let uid : String
+    let email : String
+    var postedItems : [FoodItem]
+    var requestedItems : [FoodItem]
     
+    init(uid: String, email: String) {
+        self.uid = uid
+        self.email = email
+        self.postedItems = []
+        self.requestedItems = []
+    }
     
 
 }
