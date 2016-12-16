5a6e00b0fc4e331e45978043a8cd9b43efd2e484
Suvan <suvanramani@gmail.com>
2016-12-15 18:41:15 -0800

    User creation working

--- FinalProject/LoginViewController.swift
+++ FinalProject/LoginViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func signupButton(_ sender: UIButton) {
+        let signupController = UIAlertController(title: "Sign up",
+                                                 message: "Input email and password",
+                                                 preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .destructive)
+        
+        let signupAction = UIAlertAction(title: "Register",
+                                         style: .default)
+        { action in
+            
+            let email = signupController.textFields![0]
+            let password = signupController.textFields![1]
+            
+            FIRAuth.auth()!.createUser(withEmail: email.text!, password: password.text!) { user, error in
+                if error == nil {
+                    print("signup successul!")
+                } else {
+                    print(error!.localizedDescription)
+                }
+                
+            }
+        }
+        signupController.addTextField { textEmail in
+            textEmail.placeholder = "Enter your email address"
+        }
+        signupController.addTextField { textPassword in
+            textPassword.isSecureTextEntry = true
+            textPassword.placeholder = "Enter your password"
+        }
+        signupController.addAction(signupAction)
+        signupController.addAction(cancelAction)
+        present(signupController, animated: true)
+    }
+
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+        
+    }
+    
     
     
 
