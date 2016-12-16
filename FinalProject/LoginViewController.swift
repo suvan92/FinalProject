067cfccdb66700ec1f60f463c5dcaab5ce1fff83
Suvan <suvanramani@gmail.com>
2016-12-15 17:46:20 -0800

    Create Storyboard hierarchy and loginVC

---
+++ FinalProject/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  FinalProject
+//
+//  Created by Suvan Ramani on 2016-12-15.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
