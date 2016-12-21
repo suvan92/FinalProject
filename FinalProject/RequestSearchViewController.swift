aaa7b0e13d2c2c72564ce01eca1f982af6cf619a
Suvan <suvanramani@gmail.com>
2016-12-20 20:05:25 -0800

    Add search bar and segue to searchResultsVC

--- FinalProject/RequestSearchViewController.swift
+++ FinalProject/RequestSearchViewController.swift
@@ -8,28 +8,18 @@
 
 import UIKit
 
-class RequestSearchViewController: UIViewController {
+let requestVCTitle = "Active Requests"
+let searchButtonSegueIdentifier = "searchButtonSegue"
+
+class RequestSearchViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.title = requestVCTitle
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        performSegue(withIdentifier: searchButtonSegueIdentifier, sender: self)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
