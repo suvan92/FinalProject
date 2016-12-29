ef61525974665fbcffb4dff268d649bbd7d70b20
Tim Beals <tim.beals@gmail.com>
2016-12-29 15:46:44 -0800

    apply styling elements

---
+++ FinalProject/ColorManager.swift
@@ -0,0 +1,21 @@
+//
+//  ColorManager.swift
+//  FinalProject
+//
+//  Created by Tim Beals on 2016-12-29.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+import UIKit
+
+class ColorManager: NSObject {
+
+    class func red() -> UIColor {
+        return UIColor(red: 193/255, green: 30/255, blue: 30/255, alpha: 1)
+    }
+    
+    class func lightRed() -> UIColor {
+        return UIColor(red: 253/255, green: 178/255, blue: 178/255, alpha: 1)
+    }
+    
+}
