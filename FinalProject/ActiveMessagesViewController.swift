3cc67834273346a5e8d52065576b842970fcf45c
Tim Beals <tim.beals@gmail.com>
2016-12-31 21:46:11 -0800

    get array of user channels from posted food items

--- FinalProject/ActiveMessagesViewController.swift
+++ FinalProject/ActiveMessagesViewController.swift
@@ -12,17 +12,18 @@ import Firebase
 class ActiveMessagesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-    var postedItemsChannels: [Channel]?
+    var postedItemsChannels: [Channel]? = []
     var requestedItemsData: [FoodItem]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.tabBar.isHidden = false
-
+        
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = false
+        getPostItems()
     }
     
     //MARK: tableview datasource methods
@@ -47,8 +48,6 @@ class ActiveMessagesViewController: UIViewController, UITableViewDelegate, UITab
 //        self.performSegue(withIdentifier: "ShowChannel", sender: channelId)
     }
 
-    
-    
     func getPostItems() {
         let currentUser = User.sharedInstance
         let currentUserRef = userRef.child(currentUser.uid!)
@@ -59,22 +58,35 @@ class ActiveMessagesViewController: UIViewController, UITableViewDelegate, UITab
                 foodRef.child(itemReferenceValue).observe(.value, with: { snap in
                     let foodItem = FoodItem(snapshot: snap)
                     postedItems.append(foodItem)
+                    print("FOOD ITEM: \(foodItem.name) POSTED ITEMS COUNT: \(postedItems.count)")
                     self.getPostChannels(foodItems: postedItems)
                 })
             }
         })
     }
     
-    //you are here!! ***
+    //you are here!! *** WHY DOES THE CHANNEL GET ADDED TWICE?
     func getPostChannels(foodItems: [FoodItem]) {
-        var channels: [String] = []
+        var channelRefs: [String] = []
         for item in foodItems {
-                    
-            channels.append(item.channel)
+            if item.requesterChosen {
+                channelRefs.append(item.channel)
+            }
         }
-        
-        
+        print("CHANNEL REFS COUNT: \(channelRefs.count)")
+        for ref in channelRefs {
+            getChannel(with: ref, completion: { (channel) in
+                self.postedItemsChannels?.append(channel)
+                print("CHANNEL ADDED: \(channel.id). CHANNEL COUNT: \(self.postedItemsChannels?.count)")
+            })
+        }
     }
     
-    
+    //MARK: setupChannel
+    func getChannel(with id: String, completion: @escaping(Channel) -> Swift.Void) {
+        chanRef.child(id).observeSingleEvent(of: .value, with: { (snapshot) in
+            let channel = Channel(snapshot: snapshot)
+            completion(channel)
+        })
+    }
 }
