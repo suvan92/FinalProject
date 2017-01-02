b474a5702066e24277cd158cc8c76985790d3041
Tim Beals <tim.beals@gmail.com>
2017-01-02 11:37:43 -0800

    create tab bar icons

---
+++ FinalProject/TabBarController.swift
@@ -0,0 +1,27 @@
+//
+//  TabBarController.swift
+//  FinalProject
+//
+//  Created by Tim Beals on 2017-01-02.
+//  Copyright © 2017 suvanr. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //custom buttons
+        
+        
+    
+    
+    }
+
+    
+
+
+
+}
